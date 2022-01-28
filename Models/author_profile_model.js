41e377a79758467bc0a87eaac87433319b1f1d14
smiethereens23nas <nicholas23smith@gmail.com>
2022-01-28 12:54:19 -0500

    Working MVP

--- Models/author_profile_model.js
+++ Models/author_profile_model.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const authorSchema = new mongoose.Schema({
-  image: {
+  image: [ {
     type: String,
     default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-  },
+  }],
   name: {
     type: String,
     required: [true, "Please Enter a Valid Username."]
